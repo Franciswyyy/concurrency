a5f335a1ace4aa14e58951efa09b7935a6b82e01
wangyu <francis_wangyu@163.com>
2018-05-17 15:00:19 +0800

    add unsafepublish

---
+++ src/main/java/com/wang/concurrency/example/publish/UnsafePublish.java
@@ -0,0 +1,27 @@
+package com.wang.concurrency.example.publish;
+
+
+import com.wang.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+@Slf4j
+@NotThreadSafe
+public class UnsafePublish {
+
+    private String[] states = {"a", "b", "c"};
+
+    //通过这个方法能得到私有域的引用，其他线程可能会修改。
+    public String[] getStates() {
+        return states;
+    }
+
+    public static void main(String[] args) {
+        UnsafePublish unsafePublish = new UnsafePublish();
+        log.info("{}", Arrays.toString(unsafePublish.getStates()));
+
+        unsafePublish.getStates()[0] = "d";
+        log.info("{}", Arrays.toString(unsafePublish.getStates()));
+    }
+}
